b6364299b5e05702dfad49c8d17643e44227b58f
dinara-ubuntu <dinara@kaist.ac.kr>
2017-06-08 18:39:33 +0900

    fetch data by row from db

--- pymysql_test.py
+++ pymysql_test.py
@@ -16,5 +16,23 @@ data = cursor.fetchone()
 
 print ("Database version : %s " % data)
 
+sql = "SELECT * FROM SITE WHERE cid = '%d'" % (3)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      sid = row[0]
+      cid = row[1]
+      title = row[2]
+      desc = row[3]
+      url = row[4]
+      # Now print fetched result
+      print ("sid = %d,cid = %d,title = %s,desc = %s,url = %s" % \
+             (sid, cid, title, desc, url ))
+except:
+   print ("Error: unable to fetch data")
+
 # disconnect from server
 db.close()
